ee8f3774e52c51620b1fdb870e6b7ce2013d96fe
Lelovics László <leloka2000@gmail.com>
2023-10-30 20:47:40 +0100

    Sablon nem teljes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
       </div>
     </nav>
     <div class="container">
       <div>
-        <button @click="fetchData">Fetch Data</button>
-        <div v-if="apiResponse">{{ apiResponse }}</div>
+        <h1>Kategóriák</h1>
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Több</option>
+          <option value="1">Tech</option>
+          <option value="2">Világ</option>
+          <option value="3">Üzlet</option>
+        </select>
+        <div class="tech">
+          <h2>Technológia</h2>
+        </div>
+        
     </div>
   </div>
   </div>
@@ -42,12 +61,16 @@ export default {
       .catch(error => {
         console.log(error);
       });
-  }
+  },
+
+  
 }
 
 }
 
 </script>
+
+
 
 
 
@@ -66,6 +89,13 @@ export default {
   margin-top: 60px;
   margin: 0;
   padding: 0;
+}
+
+.carousel-inner .carousel-item.active,
+.carousel-inner .carousel-item-start,
+.carousel-inner .carousel-item-next,
+.carousel-inner .carousel-item-prev {
+  display: flex;
 }
 
 </style>
